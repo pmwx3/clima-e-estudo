2634b1438fe89ee24761290135b5a0832338b5b0
Paulo Marcelo <p.marcelo92@gmail.com>
2025-11-23 12:09:07 -0300

    feat:inclusão da função principal que será executada quando a página carregar, e função principal para atualizar os dados do painel

---
+++ _cdn/js/app.js
@@ -0,0 +1,58 @@
+// Função principal que será executada quando a página carregar
+async function carregarDadosClima() {
+  // coordenadas para sengés - pr
+  const latitude = -24.11;
+  const longitute = -49.46;
+
+  // 1 - Montar a URL da API com todos os dados que precisamos
+  // Dados atuais, horários e diários em uma única chamada
+
+  const apiUrl = `https://api.open-meteo.com/v1/forecast?latitude=${latitude}&longitude=${longitude}&current=temperature_2m,apparent_temperature,precipitation_probability,wind_speed_10m,uv_index&hourly=temperature_2m,weather_code&daily=weather_code,temperature_2m_max,temperature_2m_min&timezone=America/Sao_Paulo`;
+
+  try {
+    // 2 - Fazer a requisição para a API
+    const response = await fetch(apiUrl);
+    const data = await response.json();
+
+    // se a API retornar um erro, exibimos no console
+    if (data.error) {
+      throw new Error(data.reason);
+    }
+
+    atualizarDadosAtuais(data);
+    atualizarPrevisaoHoraria(data);
+    atualizarPrevisaoSemanal(data);
+  } catch (error) {
+    console.error("Erro ao buscar dados do clima:", error);
+    alert(
+      "Não foi possível carregar os dados do clima. Tente novamente mais tarde;"
+    );
+  }
+}
+
+// Função par atualizar o painel principal com os dados atuais
+function atualizarDadosAtuais(data) {
+  // vamos pegar os dados atuais do objeto 'data'
+  const currentWeather = data.current;
+
+  // Atualizar cada elemento no HTML com o ID correspondente
+  document.getElementById("city-name").textContent = "Sengés, PR"; // Por hora é fixo
+  document.getElementById("current-temp").textContent = `${Math.round(
+    currentWeather.temperature_2m
+  )}°`;
+  document.getElementById("apparent-temp").textContent = `${Math.round(
+    currentWeather.apparent_temperature
+  )})°}`;
+  document.getElementById(
+    "rain-chance"
+  ).textContent = `${currentWeather.precipitation_probability}%`;
+  document.getElementById(
+    "chance-of-rain-main"
+  ).textContent = `Chance de Chuva ${currentWeather.precipitation_probability}%}`;
+  document.getElementById(
+    "wind-speed"
+  ).textContent = `currentWeather.wind_speed_10m`;
+  document.getElementById("uv-index").textContent = Math.round(
+    currentWeather.uv_index
+  );
+}
